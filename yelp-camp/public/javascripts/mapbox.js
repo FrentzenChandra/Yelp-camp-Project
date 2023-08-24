bf8917517e1ff09275c14f634caffbf999a5ea74
Acen <frentzenpp@gmail.com>
2023-08-24 22:40:34 +0700

    Menambahkan cluster map

--- yelp-camp/public/javascripts/mapbox.js
+++ yelp-camp/public/javascripts/mapbox.js
@@ -6,4 +6,6 @@ const map = new mapboxgl.Map({
   zoom: 9, // starting zoom
 });
 
-const marker1 = new mapboxgl.Marker().setLngLat(campground.geometry.coordinates).addTo(map);
+const popup = new mapboxgl.Popup({ offset: 25 }).setHTML(`<h3>${campground.title}</h3><p>${campground.location}</p>`);
+
+const marker1 = new mapboxgl.Marker().setLngLat(campground.geometry.coordinates).setPopup(popup).addTo(map);
--- yelp-camp/seeds/index.js
+++ yelp-camp/seeds/index.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 const { Campground } = require("../models/camp.js");
-const { User } = require('../models/user.js');
+const { User } = require("../models/user.js");
 const cities = require("./cities.js");
 const { descriptors, places } = require("./seedHelpers.js");
-const userSeeds = require('./user.js')
+const userSeeds = require("./user.js");
 
 main().catch((err) => console.log(err));
 
@@ -14,45 +14,58 @@ async function main() {
 }
 
 const userRandomize = async (seed) => {
-  for(let i = 1 ; i <=  20 ; i++){
-  const userData = seed[i];
-  const {password,username,email} = userData;
-  const userModel = new User({username,email});
-  const user = await User.register(userModel,password)
-  console.log(user);
+  for (let i = 1; i <= 20; i++) {
+    const userData = seed[i];
+    const { password, username, email } = userData;
+    const userModel = new User({ username, email });
+    const user = await User.register(userModel, password);
+    console.log(user);
   }
-}
-
+};
 
 const makeCampground = async () => {
   const random = (e) => {
     return Math.floor(Math.random() * e.length) + 1;
   };
+  const random1000 = () => { return Math.floor(Math.random() * 1000) + 1};
   for (let i = 1; i <= 20; i++) {
     const userData = await User.find();
-    const user= userData[random(userData)].id;
+    const user = userData[random(userData)].id;
     const number = random(cities);
     const location = `${cities[number].city},${cities[number].state}`;
     const title = `${descriptors[random(descriptors)]} ${places[random(places)]}`;
     const images = [
-  {
-    url: 'https://res.cloudinary.com/dsme2ifqy/image/upload/v1692226873/Yelp-camp/cydecrfoseonnnbvsddf.jpg',
-    filename: 'Yelp-camp/cydecrfoseonnnbvsddf'
-  },
-  {
-    url: 'https://res.cloudinary.com/dsme2ifqy/image/upload/v1692226873/Yelp-camp/ldlhxws6uhubrh2itx9c.jpg',
-    filename: 'Yelp-camp/ldlhxws6uhubrh2itx9c'
-  },
-  {
-    url: 'https://res.cloudinary.com/dsme2ifqy/image/upload/v1692226873/Yelp-camp/n1x3lgusmricuzguofgn.jpg',
-    filename: 'Yelp-camp/n1x3lgusmricuzguofgn'
-  }
-];
-    const price = Math.round((Math.random())*100);
-    const description = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus repellendus natus laudantium tenetur fugiat cupiditate, et quod dolore perspiciatis nulla praesentium optio impedit ex doloribus rerum, libero consectetur distinctio qui?";
-    const campground = new Campground({location,title,images,price,description,user});
-    await campground.save(); 
+      {
+        url: "https://res.cloudinary.com/dsme2ifqy/image/upload/v1692226873/Yelp-camp/cydecrfoseonnnbvsddf.jpg",
+        filename: "Yelp-camp/cydecrfoseonnnbvsddf",
+      },
+      {
+        url: "https://res.cloudinary.com/dsme2ifqy/image/upload/v1692226873/Yelp-camp/ldlhxws6uhubrh2itx9c.jpg",
+        filename: "Yelp-camp/ldlhxws6uhubrh2itx9c",
+      },
+      {
+        url: "https://res.cloudinary.com/dsme2ifqy/image/upload/v1692226873/Yelp-camp/n1x3lgusmricuzguofgn.jpg",
+        filename: "Yelp-camp/n1x3lgusmricuzguofgn",
+      },
+    ];
+    const price = Math.round(Math.random() * 100);
+    const description =
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus repellendus natus laudantium tenetur fugiat cupiditate, et quod dolore perspiciatis nulla praesentium optio impedit ex doloribus rerum, libero consectetur distinctio qui?";
+    const geometry = {
+      type: "Point",
+      coordinates: [cities[random1000()].longitude, cities[random1000()].latitude],
+    };
+    const campground = new Campground({ location, title, images, price, description, user, geometry });
+    await campground.save();
   }
 };
 
+const deleteAllCampground = async () => {
+  const campgrounds = await Campground.find({});
+  for(let campground of campgrounds){
+    const id  = campground.id;
+    await Campground.findByIdAndDelete(id);
+  }
+}
 
+makeCampground();
